ee3068ca1137e3a29ccfd05e85f03a68cf3ce349
EC2 Default User <ec2-user@ip-172-31-4-33.ec2.internal>
2023-01-06 09:23:05 +0000

    [update] comment fanction

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,32 @@
+class BookCommentsController < ApplicationController
+  def create
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    if @comment.save
+      flash[:notice] = "You have created comment successfully."
+      redirect_to book_path(@book.id)
+    else
+      @book_new = Book.new
+      @book_comments = @book.book_comments
+      @book_comment = BookComment.new
+      render template: "books/show"
+    end
+  end
+  
+  def destroy
+    book_comment = BookComment.find(params[:id])
+    if book_comment.user_id != current_user.id
+      redirect_to book_path(book_comment.book_id)
+    else
+      book_comment.destroy
+      render request.referer
+    end
+  end
+  
+  private 
+  
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,13 @@
 Rails.application.routes.draw do
-  # get 'users/show'
-  # get 'users/edit'
-  # get 'users/update'
-  # get 'books/create'
-  # get 'books/edit'
-  # get 'books/destory'
-  # get 'books/update'
-  # get 'books/index'
-  # get 'books/show'
-  # get 'homes/top'
+
   devise_for :users
   
   root to: "homes#top"
     
   resources :users, only: [:show, :edit, :update, :index, :create]
-  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
+    resources :book_comments, only: [:create, :destroy]
+  end
   
   get 'home/about' => 'homes#about', as: 'about'
   patch 'users/:id' => 'users#update', as: 'update_user'
