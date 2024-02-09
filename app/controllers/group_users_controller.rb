ba566789dc2dd8673ff4fd13be580ea0e628de60
Kii120 <117469095+Kii120@users.noreply.github.com>
2024-02-09 19:49:35 +0900

    finish 8c

--- app/controllers/group_users_controller.rb
+++ app/controllers/group_users_controller.rb
@@ -1,18 +1,20 @@
 class GroupUsersController < ApplicationController
-  def creat
+  def create
+    @group = Group.find(params[:group_id])
     @group_user = GroupUser.new
     @group_user.user_id = current_user.id
     @group_user.group_id = @group.id
     if @group_user.save
-      redirect_to contoroller: :Group, action: :index
+      redirect_to controller: :groups, action: :index
     else
       render :back
     end
   end
 
   def destroy
+    group = Group.find(params[:id])
     group_user = GroupUser.find_by(user_id: current_user, group_id: group.id)
     group_user.destroy
-    redirect_to contoroller: :Group, action: :index
+    redirect_to controller: :groups, action: :index
   end
 end
